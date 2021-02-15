464f09da5f6ea0c529ea4a8c63882e3d8251091e
reyes2981 <noe.reyes92@yahoo.com>
2021-02-15 01:38:11 -0600

    create a login view file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get "/" do
+    binding.pry
     erb :welcome
   end
 
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+    get '/login' do
+        erb :"/users/login.html"
+    end
+
+    # this method does not login user it confirms that the user entered in the correct information for login
+    post '/login' do
+        @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/'
+        else
+            erb :'users/login.html'
+        end
+    end
+
+end
