47030c510e59d3d52464c9f02ed78de90e222d1e
reyes2981 <noe.reyes92@yahoo.com>
2021-02-16 11:43:55 -0600

    added routes to log controller/added an index file to views/logs folder

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -4,12 +4,25 @@ class LogsController < ApplicationController
         authentication_required
     end
 
+    get '/logs' do
+        @logs = Log.find_by(:user => current_user)
+        erb :"logs/index.html"
+    end
+
     get '/logs/new' do
         erb :"logs/new.html"
     end
 
-    get '/logs/1' do
-        "Hello"
+    post '/logs' do
+        @log = Log.new
+        @log.content = params[:content]
+        @log.user = current_user
+
+        if @log.save
+            redirect '/logs'
+        else    
+            erb :"playlists/new.html"
+        end
     end
 
 end
