165f22c3496db505cf6921c08c0c8d20fdb1a13d
reyes2981 <noe.reyes92@yahoo.com>
2021-02-16 23:05:27 -0600

    added a username column to schema

---
+++ db/migrate/20210217045241_create_username_for_users.rb
@@ -0,0 +1,5 @@
+class CreateUsernameForUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_140824) do
+ActiveRecord::Schema.define(version: 2021_02_17_045241) do
 
   create_table "logs", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_140824) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "username"
   end
 
 end
