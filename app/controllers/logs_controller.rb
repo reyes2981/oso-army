30c90095335e46a5e568214e1cb8834b68afbd71
reyes2981 <noe.reyes92@yahoo.com>
2021-02-18 14:39:57 -0600

    added an edit route in log controller and a edit file in views/logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -31,8 +31,14 @@ class LogsController < ApplicationController
         if @log.save
             redirect '/logs'
         else    
-            erb :"playlists/new.html"
+            erb :"logs/new.html"
         end
+    end
+
+    get '/logs/:id/edit' do # I need to edit a specific log 
+        # I want to render a form to edit a log 
+        @log = current_user.logs.find(params[:id]) # need to locate log before I can edit it.
+        erb :"logs/edit.html"
     end
 
 end
