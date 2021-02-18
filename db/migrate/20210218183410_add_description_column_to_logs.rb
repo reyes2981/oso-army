3e39ef110560b3a4575a780466676ff10d558623
reyes2981 <noe.reyes92@yahoo.com>
2021-02-18 12:35:16 -0600

    added description column to logs

---
+++ db/migrate/20210218183410_add_description_column_to_logs.rb
@@ -0,0 +1,5 @@
+class AddDescriptionColumnToLogs < ActiveRecord::Migration[5.2]
+  def change
+    add_column :logs, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_182528) do
+ActiveRecord::Schema.define(version: 2021_02_18_183410) do
 
   create_table "logs", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
